2fb0ed849b306ca940318ba5a219d849715909bb
XirusTheVirus <galbru16@gmail.com>
2021-03-09 08:54:26 +0200

    Adding documentation to react.go file

--- react.go
+++ react.go
@@ -7,45 +7,53 @@ import (
 	guuid "github.com/google/uuid"
 )
 
+// Holds the graph structure
 var g *graph.Graph = &graph.Graph{Vertices: make(map[string]*graph.Vertex)}
-var callbacksCounter int = 0
-var tempCallback []func(int)
 
 type React struct{}
 
+// Gcell struct acts as father of ICell (input) & CCell (compute) and holds their common attributes
 type GCell struct {
 	value int
 	id    string
 }
 
+// Defines InputCell interface behavior
 type ICell struct {
-	GCell
+	GCell // Inherit Gcell properties
 }
 
+// Defines ComputeCell interface  behavior
 type CCell struct {
-	GCell
-	inputs    []Cell
-	callbacks []*Callback
-	compute1  func(int) int
-	compute2  func(int, int) int
+	GCell                        // Inherits Gcelll properties
+	inputs    []Cell             // Stores all compute cell's independent cells
+	callbacks []*Callback        // Stores all compute cell calbacks
+	compute1  func(int) int      // Stores computation function with one independent
+	compute2  func(int, int) int // Stores computation function with two independents
 }
 
+// Defines Cancel interface behavior
 type CCancel struct {
 	callback *Callback
 }
 
+// Defines a callback structure for CCancel entity
 type Callback struct {
-	f func(int)
+	f func(int) // Stores the CCancel entity callback
 }
 
+// Returns cell's id (string)
 func (c *GCell) GetId() string {
 	return c.id
 }
 
+// Generates unique id for the cell's id to avoid collision in the graph's map holds all cells
+// Returns string
 func (r React) GenerateUniqueId() string {
 	return guuid.New().String()
 }
 
+// Setts the value of the cell
 func (c *GCell) SetValue(value int) {
 	if c.value != value {
 		c.value = value
@@ -53,29 +61,43 @@ func (c *GCell) SetValue(value int) {
 	}
 }
 
+// This function responsible to update all the desired cells, according to their dependencies cells value modification.
+// The values modification process goes as follow:
+// 1. Send the cells graph to a topological sorting
+// 2. Go through the sorted array
+// 3. Update cell value according to its correct order
 func propagate() {
+	// The graph received from the topological sort
 	sortedGraph := g.GetGraphDependenies()
 	for _, v := range sortedGraph {
+		// Input cells shouldn't be updated at all, only via 'SetValue' function
 		if reflect.TypeOf(v.GetCell()).String() == "*react.CCell" {
 			currentVal := v.GetCell().(*CCell).Value()
+			// Determine to which compute function we should call, according to its list of parameter
 			switch len(v.GetCell().(*CCell).inputs) {
 			case 1:
+				// Update cell value
 				v.GetCell().(*CCell).value = v.GetCell().(*CCell).compute1(getValueByType(v.GetCell().(*CCell).inputs[0]))
+				// Call the callback to notify cell value modification
 				executeCallback(v.GetCell(), currentVal)
 			case 2:
+				// Update cell value
 				v.GetCell().(*CCell).value = v.GetCell().(*CCell).compute2(getValueByType(v.GetCell().(*CCell).inputs[0]), getValueByType(v.GetCell().(*CCell).inputs[1]))
+				// Call the callback to notify cell value modification
 				executeCallback(v.GetCell(), currentVal)
 			}
 		}
 	}
 }
 
+// Adds callback to designated compute cell
 func (c *CCell) AddCallback(callback func(value int)) Canceler {
 	function := &Callback{f: callback}
 	c.callbacks = append(c.callbacks, function)
 	return &CCancel{callback: function}
 }
 
+// Executes callback compute cell callback
 func executeCallback(c interface{}, currentVal int) {
 	if shouldExecuteCallback(c, currentVal) == true {
 		callbacks := c.(*CCell).callbacks
