a79d22c0de2bbe9baa27a739cbed154c352170c2
XirusTheVirus <galbru16@gmail.com>
2021-04-13 07:23:35 +0300

    Creating Open, Close and Balance functions

--- bank-account/bank_account.go
+++ bank-account/bank_account.go
@@ -5,5 +5,23 @@ type Account struct {
 }
 
 func Open(balance int64) *Account {
+	if balance < 0 {
+		return nil
+	}
+
 	return &Account{balance: balance}
+}
+
+func (a *Account) Close() (int64, bool) {
+	balance := a.balance
+	a = nil
+	return balance, true
+}
+
+func (a *Account) Balance() (int64, bool) {
+	if a.balance < 0 {
+		return a.balance, false
+	}
+
+	return a.balance, true
 }
